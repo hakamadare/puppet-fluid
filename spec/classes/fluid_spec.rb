21b434e5c7828bebc783a8bd05a6c4ba283576a9
Steve Huff <shuff@vecna.org>
2014-05-08 19:08:21 -0400

    fixing unit test

--- spec/classes/fluid_spec.rb
+++ spec/classes/fluid_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe 'fluid' do
-    it do
-      should contain_package('Fluid-1.7.2').with({
-        :provider => 'compressed_app',
-        :source   => 'http://www.fluidapp.com/dist/Fluid_1.7.2.zip',
-      })
-    end
+  let (:params) {{:version => '1.8.2'}}
+  it do
+    should contain_package('Fluid').with({
+      :provider => 'compressed_app',
+      :source   => 'http://www.fluidapp.com/dist/Fluid_1.8.2.zip',
+    })
+  end
 end
